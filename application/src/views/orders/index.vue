6ac20370ea73fa5e10197f42b91139a60131a5eb
Tobias Artz <tobiasartz@gmail.com>
2020-07-07 11:01:23 -0700

    init

---
+++ application/src/views/orders/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="component">
+        <h1>Boilerplate View</h1>
+    </div>
+</template>
+
+<script type="text/babel">
+    export default {
+        name: 'views-_boilerplate-index',
+        watch: {},
+        props: {},
+        data() {
+            return {}
+        },
+        methods: {},
+        computed: {},
+        created() {
+        },
+        mounted() {
+        },
+        beforeDestroy() {
+        },
+        mixins: [],
+        components: {}
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
