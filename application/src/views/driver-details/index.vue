ad2af4e4d69081be4c04fe46ead1be231176c1ab
santiago0624 <gezo64@hotmail.com>
2021-01-29 02:21:48 -0500

    US#4 Implementing Initial logic for displaying the driver-details components.

---
+++ application/src/views/driver-details/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="component">
+    <div class="container--fluid">
+      <h1>Driver {{ this.$route.params.driverId }} Details</h1>
+      <div v-if="drivers">
+        DETAILS FOR USER!!
+        {{ this.$route.params }}
+        {{ drivers }}
+      </div>
+      <div v-if="!drivers" class="loader-container">
+        <div class="loader"></div>
+        <div class="loader-text">Loading Driver Details ...</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/babel">
+import DriversService from "@/services/drivers";
+
+export default {
+  name: "views-driver-details",
+  watch: {},
+  props: {},
+  data() {
+    return {
+      drivers: undefined,
+    };
+  },
+  methods: {
+    getDrivers() {
+      return DriversService.getDrivers().then((result) => {
+        this.drivers = result;
+      });
+    },
+  },
+  computed: {},
+  created() {},
+  mounted() {
+    this.getDrivers();
+  },
+  beforeDestroy() {},
+  mixins: [],
+  components: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.loader-container {
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+  justify-content: center;
+  align-items: center;
+}
+
+.loader {
+  border: 10px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 10px solid #32b2db;
+  width: 80px;
+  height: 80px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 1s linear infinite;
+}
+
+.loader-text {
+  margin-top: 20px;
+  font-size: 20px;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
