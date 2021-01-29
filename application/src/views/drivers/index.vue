47ef599185b669ac930f3f75431a5cf7a3eb3580
santiago0624 <gezo64@hotmail.com>
2021-01-28 23:47:45 -0500

    US#1 Implementing loading indicator for whenever data is being retrieve.

--- application/src/views/drivers/index.vue
+++ application/src/views/drivers/index.vue
@@ -1,49 +1,81 @@
 <template>
-    <div class="component">
-        <div class="container--fluid">
-            <h1>Drivers</h1>
-            {{ result }}
-        </div>
+  <div class="component">
+    <div class="container--fluid">
+      <h1>Drivers</h1>
+      <div v-if="result">
+        {{ result }}
+      </div>
+
+      <div v-if="!result" class="loader-container">
+        <div class="loader"></div>
+        <div class="loader-text">Loading Drivers ...</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script type="text/babel">
-    import DriversService from '@/services/drivers'
+import DriversService from "@/services/drivers";
 
-    export default {
-        name: 'views-orders-index',
-        watch: {},
-        props: {},
-        data() {
-            return {
-                result: undefined
-            }
-        },
-        methods: {
-            getDrivers() {
-                return DriversService.getDrivers()
-                    .then((result) => {
-                        this.result = result;
-                    })
-            },
-        },
-        computed: {
-
-        },
-        created() {
-        },
-        mounted() {
-            this.getDrivers();
-        },
-        beforeDestroy() {
-        },
-        mixins: [],
-        components: {}
-    }
-
+export default {
+  name: "views-orders-index",
+  watch: {},
+  props: {},
+  data() {
+    return {
+      result: undefined,
+    };
+  },
+  methods: {
+    getDrivers() {
+      return DriversService.getDrivers().then((result) => {
+        this.result = result;
+      });
+    },
+  },
+  computed: {},
+  created() {},
+  mounted() {
+    this.getDrivers();
+  },
+  beforeDestroy() {},
+  mixins: [],
+  components: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
+.loader-container {
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+  justify-content: center;
+  align-items: center;
+}
+
+.loader {
+  border: 10px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 10px solid #32b2db;
+  width: 80px;
+  height: 80px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 1s linear infinite;
+}
+
+.loader-text {
+  margin-top: 20px;
+  font-size: 20px;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
