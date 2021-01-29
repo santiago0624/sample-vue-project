9526ce1bacac004d26f3ca26513d421f8d795576
santiago0624 <gezo64@hotmail.com>
2021-01-29 10:37:48 -0500

    BONUS# Removing logic from the List and included the app-list definition instead.

--- application/src/views/drivers/index.vue
+++ application/src/views/drivers/index.vue
@@ -4,24 +4,7 @@
       <h1 class="ml-5">Drivers</h1>
 
       <div v-if="result" id="driverListView">
-        <div class="drivers-list-container" id="driverListConatiner">
-          <div class="driver-search">
-            <input class="form-control driver-search-input" type="search" v-model="searchQuery"
-                   placeholder="Search Driver" id="driverSearch"/>
-          </div>
-          <div v-for="driver in resultQuery()" :key="driver.id" class="drivers-list-row" 
-               @click="navigateToDriverDetails(driver.id)" id="driver.id">
-            <i class="glyphicon glyphicon-user"></i>
-            <div class="driver-name" :style="changer" id="driverName">
-              {{ driver.first_name }}
-            </div>
-            <app-avatar :driver="driver"></app-avatar>
-          </div>
-          <div class="select-color-section">
-             <label for="color">Select Font Color: </label>
-             <input id="color" class="color-picker" type="color" v-model="changer.color"/>
-          </div>
-        </div>
+        <app-list :drivers="result"></app-list>
       </div>
 
       <div v-if="!result" class="loader-container" id="loaderComponent">
@@ -34,7 +17,7 @@
 
 <script type="text/babel">
 import DriversService from "@/services/drivers";
-import Avatar from "@/components/avatar";
+import List from "@/components/list";
 
 export default {
   name: "views-orders-index",
@@ -42,11 +25,7 @@ export default {
   props: {},
   data() {
     return {
-      result: undefined,
-      searchQuery: undefined,
-      changer:{
-        color: "#000000"
-      }
+      result: undefined
     };
   },
   methods: {
@@ -54,22 +33,7 @@ export default {
       return DriversService.getDrivers().then((result) => {
         this.result = result;
       });
-    },
-    navigateToDriverDetails(id) {
-      this.$router.push({ name: "driver-details", params: { driverId: id } });
-    },
-    resultQuery() {
-      if (this.searchQuery) {
-        return this.result.filter((driver) => {
-          return this.searchQuery
-            .toLowerCase()
-            .split(" ")
-            .every((value) => driver.first_name.toLowerCase().includes(value));
-        });
-      } else {
-        return this.result;
-      }
-    },
+    }
   },
   computed: {},
   created() {},
@@ -79,13 +43,14 @@ export default {
   beforeDestroy() {},
   mixins: [],
   components: {
-    "app-avatar": Avatar,
+    "app-list": List
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
 .loader-container {
   display: flex;
   flex-direction: column;
@@ -116,68 +81,5 @@ export default {
   100% {
     transform: rotate(360deg);
   }
-}
-
-.driver-search {
-  padding: 20px 20px;
-  width: 320px;
-  border: 1px solid #32b2db;
-  border-radius: 10px 10px 0 0;
-}
-
-.driver-search-input {
-  height: 38px;
-  border-radius: 15px;
-}
-
-.drivers-list-container {
-  margin-top: 80px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.drivers-list-row {
-  display: flex;
-  align-items: center;
-  border: 1px solid #32b2db;
-  padding: 15px 20px;
-  width: 320px;
-  border-top: none;
-}
-
-.drivers-list-row:hover {
-  cursor: pointer;
-  background-color: #69c8e7;
-  color: white;
-}
-
-.drivers-list-row:first-child {
-  border-radius: 10px 10px 0 0;
-  border-top: none;
-}
-
-.drivers-list-row:last-child {
-  border-bottom: 1px solid #32b2db;
-  border-radius: 0 0 10px 10px;
-}
-
-.driver-name {
-  margin-left: 15px;
-  font-size: 15px;
-  width: 215px;
-}
-
-.select-color-section {
-  display: flex;
-  flex-direction: column;
-  width: 320px;
-  padding: 15px;
-  align-items: center;
-}
-
-.color-picker {
-  cursor: pointer;
 }
 </style>
